db28b8489a1f229f34f6ade42a0787b2f2ab84dd
Amy N. <144870629+amythnn@users.noreply.github.com>
2026-05-27 21:32:10 -0700

    Update config.ts (new title pt7 - final touches)

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // src/config.ts
 export const siteConfig = {
   name: "Amy N.",
-  title: "Duke Graduate Student · Georgetown & Penn State Alum\nScholar and Data Science Practitioner with Interests in Technology, Policy, & Ethics",
+  title: "Duke Graduate Student · Georgetown & Penn State Alum\nInterests in Technology, Policy, & Ethics",
   description: "Portfolio site of Amy N.",
   accentColor: "#60A5FA",
 
